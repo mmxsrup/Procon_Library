b5c7c4532ce51774f744fa77a1aa82e0df5cbcc3
yuichi.sugiyama <mmxsrup@yahoo.co.jp>
2016-12-24 18:41:25 +0900

    eratos.h first commit

---
+++ math/eratos.h
@@ -0,0 +1,14 @@
+bool isprime[20010];
+//エラトステネス
+void eratos(int m){
+    for (int i = 0; i <= m; ++i) isprime[i] = true;
+    isprime[0] = isprime[1] = false;
+    //iを残してiの倍数を消していく
+    for (int i = 2; i <= m; ++i){
+        if(isprime[i]){
+            for (int j = 2 * i; j <= m; j += i){
+                isprime[j] = false;
+            }
+        }
+    }
+}
