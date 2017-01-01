bbe2b237b6339df099438e6805a1d1a393a4a08c
yuichi.sugiyama <mmxsrup@yahoo.co.jp>
2017-01-01 21:53:33 +0900

    テンプレートヘッダファイル

---
+++ template.h
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef vector<pint> vpint;
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define reps(i,f,n) for(int i=(f);i<(n);i++)
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();it++)
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define chmax(a, b) a = (((a)<(b)) ? (b) : (a))
+#define chmin(a, b) a = (((a)>(b)) ? (b) : (a))
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
