f341fc84e31ebe5c17cec6ed6d394c3aeb2108e1
yuichi.sugiyama <mmxsrup@yahoo.co.jp>
2017-01-23 01:03:32 +0900

    経路復元のfix

--- dp/Traveling_Salesman.h
+++ dp/Traveling_Salesman.h
@@ -1,8 +1,8 @@
 const int MAX_N = 20;
 vector<pair<int, int> > G[MAX_N]; /* G[u][v] := u->v's weight */
 int dp[(1 << MAX_N)][MAX_N];
-int pre[MAX_N];
-void Traveling_Salesman(int n){ /* n := Number of vertices */
+int pre[(1 << MAX_N)][MAX_N];
+int Traveling_Salesman(int n){ /* n := Number of vertices */
 	rep(i, (1 << n))rep(j, n)dp[i][j] = INF; /* Initialization */
 	dp[0][0] = 0;
 	for (int mask = 0; mask < (1 << n); ++mask){
@@ -12,12 +12,26 @@ void Traveling_Salesman(int n){ /* n := Number of vertices */
 				if((mask & (1 << v)) == 0){
 					if(dp[mask | (1 << v)][v] > dp[mask][u] + w){
 						dp[mask | (1 << v)][v] = dp[mask][u] + w;
-						pre[v] = u;
+						pre[mask | (1 << v)][v] = u;
 					}
 				}
 			}
 		}
 	}
+	return dp[(1 << n) - 1][0];
+}
+vector<int> Restoration(int n){
+	vector<int> root;
+	int mask = (1 << n) - 1, v = 0;
+	root.push_back(v);
+	while(mask != 0){
+		int u = pre[mask][v]; /* u -> v */
+		root.push_back(u);
+		mask ^= (1 << v);
+		v = u;
+	}
+	reverse(root.begin(), root.end());
+	return root;	
 }
 
 /*
