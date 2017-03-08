db18ad4a4d648b11f5e25288ccc2156542408b9f
mmxsrup <mmxsrup@yahoo.co.jp>
2017-03-09 04:47:20 +0900

    segtree高速化

--- data_struture/segtree_max.hpp
+++ data_struture/segtree_max.hpp
@@ -11,18 +11,18 @@ public:
 	segtree(int n_): n(n_){ //n_要素数
 		n = 1;
 		while(n < n_) n *= 2;
-		dat.resize(n * 2, INF); //(1) 初期値を最大に
+		dat.resize(n * 2, 0); //(1) 初期値を最小に -INFかも
 	}
 	void update(int k, T val){ // k番目の値(0-indexed)を val に変更
 		for (dat[k += n] = val; k > 0; k >>= 1){ // kを含む区間のインデックスを下から順に列挙
-			dat[k>>1] = min(dat[k], dat[k ^ 1]); // (2) 区間の最大値で更新
+			dat[k>>1] = max(dat[k], dat[k ^ 1]); // (2) 区間の最大値で更新
 		}
 	}
 	T query(int l, int r){ //[l, r)の区間
-		T ret = INF; //(3) 最小値に関係ない値
+		T ret = 0; //(3) 最大値に関係ない値 -INFかも
 		for (l += n, r += n; l < r; l >>= 1, r >>= 1){
-			if(l & 1) ret = min(ret, dat[l++]); //(4) 区間の最小値で更新
-			if(r & 1) ret = min(ret, dat[--r]); //(4) 区間の最小値で更新
+			if(l & 1) ret = max(ret, dat[l++]); //(4) 区間の最大値で更新
+			if(r & 1) ret = max(ret, dat[--r]); //(4) 区間の最大値で更新
 		}
 		return ret;
 	}
@@ -34,6 +34,7 @@ segtree すこし高速化
 更新: 1 点
 クエリ: 区間の最小値
 note: (1)~(4)を変えることで対応可能
-http://arc045.contest.atcoder.jp/tasks/arc045_b
-(http://arc045.contest.atcoder.jp/submissions/1151765)
+http://codeforces.com/contest/777/problem/E
+(http://codeforces.com/contest/777/submission/25346678)
+この問題では差はない
 */
