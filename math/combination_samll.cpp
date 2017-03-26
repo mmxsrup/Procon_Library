8c46153cdeb0b994022c0116927e77bd29d57348
mmxsrup <mmxsrup@yahoo.co.jp>
2017-03-26 23:42:54 +0900

    fix

--- math/combination_samll.cpp
+++ math/combination_samll.cpp
@@ -1,7 +1,8 @@
+const int MAX_N = 60;
 ll com[MAX_N][MAX_N];
 void combination(void){ //com[i][j] := iCj
 	com[0][0] = 1;
-	for (int i = 1; i <= 55; ++i){
+	for (int i = 1; i <= MAX_N - 1; ++i){
 		for (int j = 0; j <= i; ++j){
 			//パスカルの３角形
 			if(j == 0) com[i][j] = com[i - 1][j];
@@ -12,4 +13,5 @@ void combination(void){ //com[i][j] := iCj
 
 /*
 http://abc057.contest.atcoder.jp/tasks/abc057_d
+(http://abc057.contest.atcoder.jp/submissions/1184866)
 */
