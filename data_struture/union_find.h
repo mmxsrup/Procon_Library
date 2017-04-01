8de6c3739243867f177f9918010fe0d6bf052d8a
mmxsrup <mmxsrup@yahoo.co.jp>
2017-04-01 12:10:11 +0900

    union find fix

--- data_struture/union_find.h
+++ data_struture/union_find.h
@@ -1,40 +1,37 @@
 class DisjointSet{
-    public:
-        vector<int> rank, p;//rank:木の高さ p:親の頂点番号
-        DisjointSet(){}
-        DisjointSet(int size){//頂点の数
-            rank.resize(size, 0);
-            p.resize(size, 0);
-            rep(i, size) makeSet(i);
-        }
-        void makeSet(int x){
-            p[x] = x;
-            rank[x] = 0;
-        }
-        //同じ木にあるか判定（今回使わない）
-        bool same(int x, int y){
-            return findSet(x) == findSet(y);
-        }
-        // 木どうしをくっつける
-        void unite(int x, int y){
-            link(findSet(x), findSet(y));
-        }
-        //木の高さを考慮して木どうしをくっつける
-        void link(int x, int y){
-            if(rank[x] > rank[y]){
-                p[y] = x;
-            }else{
-                p[x] = y;
-                if(rank[x] == rank[y]) rank[y]++;
-            }
-        }
-        //親を探す（ルートまで）
-        int findSet(int x){
-            if(x != p[x]){
-                p[x] = findSet(p[x]);
-            }
-            return p[x];
-        }
+public:
+	vector<int> rank, p;//rank:木の高さ p:親の頂点番号
+	DisjointSet(){}
+	DisjointSet(int size){//頂点の数
+		rank.resize(size, 0);
+		p.resize(size, 0);
+		rep(i, size) makeSet(i);
+	}
+	bool same(int x, int y){ //同じ木にあるか
+		return findSet(x) == findSet(y);
+	}
+	void unite(int x, int y){ // 木どうしをくっつける
+		link(findSet(x), findSet(y));
+	}
+private:
+	void makeSet(int x){
+		p[x] = x;
+		rank[x] = 0;
+	}
+	int findSet(int x){ //親を探す（ルートまで）
+		if(x != p[x]){
+			p[x] = findSet(p[x]);
+		}
+		return p[x];
+	}
+	void link(int x, int y){ //木の高さを考慮して木どうしをくっつける
+		if(rank[x] > rank[y]){
+			p[y] = x;
+		}else{
+			p[x] = y;
+			if(rank[x] == rank[y]) rank[y]++;
+		}
+	}
 };
 
 /*
