cb5d038804d42559c73305b582d3a2b4339e3958
yuichi.sugiyama <mmxsrup@yahoo.co.jp>
2017-10-22 17:16:53 +0900

    dijkstra 経路復元 ゴールまでの道順がない場合にも対応

--- graph/dijkstra_.Restoration.cpp
+++ graph/dijkstra_.Restoration.cpp
@@ -1,8 +1,8 @@
-const int MAX_N = 210;
+const int MAX_N = 1010;
 vector<pair<int, int> > G[MAX_N];
 vector<int> dijkstra(int start, int goal){//スタートとゴールを逆に
-	vector<long long> dist(MAX_N, INF);
-	vector<int> pre(MAX_N, -1);//pre[i] := iの前の頂点
+    vector<long long> dist(MAX_N, INF);
+    vector<int> pre(MAX_N, -1);//pre[i] := iの前の頂点
     dist[start] = 0;//dist[i] :=　start->iまでの最短距離
     priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > que;
     que.push(make_pair(0, start));
@@ -14,22 +14,25 @@ vector<int> dijkstra(int start, int goal){//スタートとゴールを逆に
         for (auto tmp : G[u]){
             int v = tmp.first, time = tmp.second;//隣接する頂点 その頂点まで行く時間
             if(dist[v] > dist[u] + time){//u->v
-            	dist[v] = dist[u] + time;
-            	pre[v] = u;
-            	que.push(make_pair(dist[v], v));
+                dist[v] = dist[u] + time;
+                pre[v] = u;
+                que.push(make_pair(dist[v], v));
             }else if(dist[v] == dist[u] + time){//辞書順最小
-            	pre[v] = min(pre[v], u);
+                pre[v] = min(pre[v], u);
             }
         }
     }
+    if(dist[goal] == INF) return {-1}; // start->goalに経路がない場合, {-1}
     //経路復元
     vector<int> path;
     int s = start, t = goal;
-	for (; t != s; t = pre[t]) path.push_back(t);
-	path.push_back(s);
-	return path; //start-> * -> goal startからgoalまでの最短経路(辞書順最小)
+    for (; t != s; t = pre[t]) path.push_back(t);
+    path.push_back(s);
+    reverse(path.begin(), path.end());
+    return path; //start-> * -> goal startからgoalまでの最短経路(辞書順最小)
 }
 
 /*
 http://yukicoder.me/problems/no/160
+http://arc011.contest.atcoder.jp/tasks/arc011_3
 */
